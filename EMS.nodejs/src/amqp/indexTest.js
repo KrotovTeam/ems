e972652bf1ec5fffddf0635aa12f36af7e414062
facelinker <facelinker1@gmail.com>
2018-05-25 14:20:59 +0300

    fix

--- EMS.nodejs/src/amqp/indexTest.js
+++ EMS.nodejs/src/amqp/indexTest.js
@@ -110,8 +110,10 @@ async function pushToEmsReliefModelServiceRequestsChannel(data) {
 // setTimeout(async ()=>{
 //     await pushToEmsDataNormalizationServiceChannel({
 //         messageType: ['urn:message:BusContracts:IDataNormalizationRequest'],
-//         Folder: PATH,
-//         SatelliteType: 1
+//         message:{
+//             Folder: PATH,
+//             SatelliteType: 1
+//         }
 //     });
 // }, 1000);
 
@@ -124,17 +126,19 @@ const RigthLower = '1.23';
 
 
 
-const PATH = 'F:\\TEST\\';
-
-setTimeout(async ()=>{
-    await pushToEmsDataNormalizationServiceChannel({
-        messageType: ['urn:message:BusContracts:IDataNormalizationRequest'],
-		message: {
-			Folder: PATH,
-			SatelliteType: 1
-		}
+setTimeout(async () => {
+    await pushToEmsReliefModelServiceRequestsChannel({
+        messageType: ['urn:message:BusContracts:IReliefCharacteristicRequest'],
+        message: {
+            LeftUpper: LeftUpper,
+            RigthLower: RigthLower,
+            DataFolder: DataFolder,
+            ResultFolder: ResultFolder,
+            CharacteristicTypes: [0, 1, 2]
+        }
     });
 }, 1000);
+
 
 
 module.exports = {
