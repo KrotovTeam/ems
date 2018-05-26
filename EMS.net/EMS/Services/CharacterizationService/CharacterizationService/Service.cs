c0297377a13431a7cbb314cc798689f24d3f8558
Илья Мжачев <i.mzhachev@solarl.ru>
2018-05-26 19:03:34 +0300

    Добавил пустой сервис определения характеристик

---
+++ EMS.net/EMS/Services/CharacterizationService/CharacterizationService/Service.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using BusContracts;
+using Common.Constants;
+using MassTransit.RabbitMqTransport;
+using Topshelf;
+using Topshelf.Logging;
+
+namespace CharacterizationService
+{
+    public class Service : ServiceControl
+    {
+        private BusManager.BusManager _busManager;
+        private static readonly LogWriter Logger = HostLogger.Get<Service>();
+
+        public bool Start(HostControl hostControl)
+        {
+            _busManager = new BusManager.BusManager();
+            _busManager.StartBus(GetBusConfigurations());
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            _busManager.StopBus();
+
+            return true;
+        }
+
+        private Dictionary<string, Action<IRabbitMqReceiveEndpointConfigurator>> GetBusConfigurations()
+        {
+            var busConfig = new Dictionary<string, Action<IRabbitMqReceiveEndpointConfigurator>>
+            {
+                {
+                    "queue_name", e =>
+                    {
+                    }
+                }
+            };
+            return busConfig;
+        }
+    }
+}
